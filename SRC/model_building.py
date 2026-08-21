0b710aa94be677ac2860ae42f88dd3432418ff3b
Digant Tiwari <tdigant5@gamil.com>
2026-08-21 06:56:14 +0530

    model_building

--- SRC/model_building.py
+++ SRC/model_building.py
@@ -28,7 +28,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-'''def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
@@ -43,7 +43,7 @@ logger.addHandler(file_handler)
         raise
     except Exception as e:
         logger.error('Unexpected error: %s', e)
-        raise'''
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -119,16 +119,12 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_data('params.yaml')['model_building']
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
 
-        params = {
-        "n_estimators": 100,
-        ##max_depth": 10,
-        "random_state": 42
-        }
+        
 
         clf = train_model(X_train, y_train, params)
         
